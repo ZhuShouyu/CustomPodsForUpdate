c4db3a5dc9fdf280306ee7a1a337ff547f225ccd
zhushouyu <zhushouyu@youzan.com>
2018-10-26 15:20:19 +0800

    sss

--- CustomPodsForUpdate/Classes/ReplaceMe.swift
+++ CustomPodsForUpdate/Classes/ReplaceMe.swift
@@ -1,6 +1,6 @@
 
 
-class TestClass {
+public class TestClass {
     static let shared = TestClass()
     func helloWorld() {
         print("Hello This is CustomPodsForUpdate")
---
+++ TestMM.swift
@@ -0,0 +1,15 @@
+//
+//  TestMM.swift
+//  CustomPodsForUpdate
+//
+//  Created by 守宇 on 2018/10/26.
+//
+
+import Foundation
+
+public class TestMM {
+    public static let shared = TestMM()
+    public func helloWorld() {
+        print("Hello This is CustomPodsForUpdate")
+    }
+}
